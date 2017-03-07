ca20bbf1473e46e2f073cc8f7ef2157f7e656e88
Minh-Long Do <minhlong.langos@gmail.com>
2017-03-07 21:37:25 +0000

    Custom save to disallow saving empty instance ID, include unittest

--- tokenstorage/models.py
+++ tokenstorage/models.py
@@ -1,10 +1,16 @@
 from django.db import models
+from django.db.utils import IntegrityError
 
 
 class TokenStore(models.Model):
-    instance_id = models.CharField(primary_key=True, max_length=255, null=False, blank=False)
+    instance_id = models.CharField(primary_key=True, max_length=255, blank=False, auto_created=False)
     github_token = models.CharField(max_length=255, blank=True, null=False)
     slack_token = models.CharField(max_length=255, blank=True, null=False)
 
     class Meta:
         ordering = ('instance_id',)
+
+    def save(self, *args, **kwargs):
+        if self.instance_id is None or len(self.instance_id) <= 0:
+            raise IntegrityError("Instance ID cannot be None or empty")
+        super(TokenStore, self).save(*args, **kwargs)
--- tokenstorage/tests.py
+++ tokenstorage/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from tokenstorage.models import TokenStore
+from django.db.utils import IntegrityError
 
-# Create your tests here.
+
+class TokenStoreTestCase(TestCase):
+    def setUp(self):
+        self.instance_id = 'instance_id'
+        self.github_token = 'github_token'
+        self.slack_token = 'slack_token'
+
+    def test_create_token_store_with_none_id_expect_integrity_exception(self):
+        self.assertRaises(IntegrityError,
+                          TokenStore.objects.create,
+                          instance_id=None,
+                          github_token=self.github_token,
+                          slack_token=self.slack_token)
+
+    def test_create_token_store_with_empty_id_expect_integrity_exception(self):
+        self.assertRaises(IntegrityError,
+                          TokenStore.objects.create,
+                          instance_id='',
+                          github_token=self.github_token,
+                          slack_token=self.slack_token)
+
+    def test_create_token_store_with_id_expect_pass(self):
+        TokenStore.objects.create(instance_id=self.instance_id,
+                                  github_token=self.github_token,
+                                  slack_token=self.slack_token)
+
+        token_store = TokenStore.objects.get(instance_id=self.instance_id)
+
+        self.assertEquals(token_store.instance_id, self.instance_id)
+        self.assertEquals(token_store.github_token, self.github_token)
+        self.assertEquals(token_store.slack_token, self.slack_token)
