fd80a5d978fa44d847c706bd08cd21ff31852b83
Minh-Long Do <minhlong.langos@gmail.com>
2017-03-09 19:04:25 +0000

    Make secret an environment variable (#9)

--- settingsmanagement/settings.py
+++ settingsmanagement/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'd&_9zf4=+5lup#-kd*2&t-1&b#z0$yi9wfh0046_q!)&oohd)l'
+SECRET_KEY = os.getenv('SECRET_KEY', 'd&_9zf4=+5lup#-kd*2&t-1&b#z0$yi9wfh0046_q!)&oohd)l')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
