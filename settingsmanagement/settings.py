a5dccd6752b6199eb0eae55d06ee247bf315eddc
Minh-Long Do <minhlong.langos@gmail.com>
2017-03-09 19:14:59 +0000

    Allowed hosts (#6)
    * Add allowed host
    
    * Add visual studio domain to allowed_host
    
    * Able to setup DEBUG to be False in Dockerfile

--- settingsmanagement/settings.py
+++ settingsmanagement/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY', 'd&_9zf4=+5lup#-kd*2&t-1&b#z0$yi9wfh0046_q!)&oohd)l')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'discovery-settingmanagement.azurewebsites.net',
+    '.visualstudio.com'
+]
 
 
 # Application definition
