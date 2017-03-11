698bcb355c964978d1b8a719d69ae70781d802ac
Minh-Long Do <minhlong.langos@gmail.com>
2017-03-11 18:36:33 +0000

    Fix url

--- tokenstorage/urls.py
+++ tokenstorage/urls.py
@@ -4,7 +4,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     url(r'^tokenstorage/$', view=TokenStorage.as_view()),
-    url(r'^git tokenstorage/all/$', view=TokenStorageViewSet.as_view({'get': 'list'})),
+    url(r'^tokenstorage/all/$', view=TokenStorageViewSet.as_view({'get': 'list'})),
     url(r'^tokenstorage/(?P<instance_id>\w{1,50})/$', view=TokenStorage.as_view())
 ]
 
