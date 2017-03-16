a6ba09be673729432efa38f739b3b9462c9dcb46
Minh-Long Do <minhlong.langos@gmail.com>
2017-03-16 23:46:01 +0000

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-$PROD=True
-python manage.py makemigrations
-python manage.py migrate
-docker login -u=$DOCKER_USERNAME -p=$DOCKER_PASSWORD $DOCKER_URL
-docker build -t $DOCKER_URL/settingmanage:$APP_MAJOR_VERSION.$APP_MINOR_VERSION.$TRAVIS_BUILD_NUMBER .
-docker push $DOCKER_URL/settingmanage:$APP_MAJOR_VERSION.$APP_MINOR_VERSION.$TRAVIS_BUILD_NUMBER
