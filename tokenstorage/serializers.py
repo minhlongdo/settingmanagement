a90a77e734c75e5b541be80d93c1150f7104220c
Minh-Long Do <minhlong.langos@gmail.com>
2017-03-17 22:20:10 +0000

    Add new field - codeclimate_token

--- tokenstorage/serializers.py
+++ tokenstorage/serializers.py
@@ -7,4 +7,5 @@ class TokenStoreSerializer(serializers.ModelSerializer):
     class Meta:
         model = TokenStore
         fields = ('instance_id', 'user_email', 'github_token', 'github_user',
-                  'github_org', 'slack_token', 'vsts_token', 'slack_channel')
+                  'github_org', 'slack_token', 'vsts_token', 'slack_channel',
+                  'codeclimate_token')
